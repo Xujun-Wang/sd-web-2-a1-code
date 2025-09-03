d423fa337f40ec9f41d8879f13dd8565d2c41192
Xujun-Wang <xujun.wang@edu.sait.ca>
2025-09-03 07:40:44 -0600

    finished task 3 in js

--- index.js
+++ index.js
@@ -19,11 +19,12 @@ const users = [
 // 1. Print out the names of each character in the console, then render them in the HTML list with id "names-list"
 document.addEventListener('DOMContentLoaded', () => {
   console.log("----------Exercise 1: (every name)----------");
+
   const namesList = document.getElementById('names-list');
   users.forEach(user => {
     console.log(user.name); // Print names to console
     const li = document.createElement('li');
-    li.textContent = user.name; // Print names to HTML list
+    li.textContent = user.name; // Print names to HTML page
     namesList.appendChild(li); // Append list item to the names list
   });
 });
@@ -31,6 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
 // 2. Print out the names of characters whose age is less than 40 in the console, then render them in the HTML list with id "young-characters-list"
 document.addEventListener('DOMContentLoaded', () => {
   console.log("----------Exercise 2: (names <40 yo)----------");
+
   const youngCharactersList = document.getElementById('young-characters-list');
   users.forEach(user => {
     if (user.age < 40) { // Filter for age less than 40
@@ -43,6 +45,21 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // 3. Create a reusable function that takes any array and uses logic to render a list of character names in the HTML. Use this function to populate the list with id "function-list"
+function renderNamesList(namesList, listId) {
+  const listElement = document.getElementById(listId);
+  namesList.forEach(user => {
+    console.log(user.name); // Print names to console
+    const li = document.createElement('li');
+    li.textContent = user.name;  // Print names to HTML page
+    listElement.appendChild(li); // Append list item to the function-generated list
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("----------Exercise 3: (function-list in a reusable function)----------");
+
+  renderNamesList(users, "function-list");
+});
 
 // 4. Create a function that takes an array and an age threshold parameter. The function should only display characters whose age is below the given number. Render results in the list with id "age-filter-list"
 
